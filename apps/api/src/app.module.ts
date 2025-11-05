d132bb30e1d902f719dd994247b968c169e3c7b9
g33ya <gscozzaro2004@gmail.com>
2025-11-05 13:23:17 -0500

    App module updates

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
-
 import { LinksModule } from './links/links.module';
-
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { EventsModule } from './events/events.module';
+import { ConnectionsModule } from './connections/connections.module';
+import { MatchingModule } from './matching/matching.module';
+import { SpotifyModule } from './spotify/spotify.module';
 
 @Module({
-  imports: [LinksModule],
+  imports: [LinksModule, AuthModule, UsersModule, SpotifyModule, MatchingModule, ConnectionsModule, EventsModule],
   controllers: [AppController],
   providers: [AppService],
 })
