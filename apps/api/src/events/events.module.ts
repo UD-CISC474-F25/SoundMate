8cdc7396454931e80e670c1b8fd01bf9e1ee6f82
g33ya <gscozzaro2004@gmail.com>
2025-11-05 14:13:05 -0500

    Adding PrismaService into connections/events modules

--- apps/api/src/events/events.module.ts
+++ apps/api/src/events/events.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   controllers: [EventsController],
-  providers: [EventsService]
+  providers: [EventsService, PrismaService],
 })
 export class EventsModule {}
